33409a74eb2802610f99b2f8a74c90361520021c
zackenichi <thekenichi26@gmail.com>
2023-09-13 22:03:26 +0800

    basic dashboard setup and theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container } from '@mui/material';
+import { Cashflow } from './Components/Cashflow';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" data-testid="app-container">
+      <Box
+        sx={{
+          flexGrow: 1,
+          alignItems: 'center',
+          flexDirection: 'column',
+          padding: 4,
+        }}
+      >
+        <Cashflow />
+      </Box>
+    </Container>
   );
 }
 
